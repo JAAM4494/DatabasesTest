6454c151c0156462e4fbfb5caec6dcc8fda5d835
JAAM4494 <ariasmendez44@gmail.com>
2015-10-07 20:18:26 -0600

    Se agrega la interfaz gráfica...

--- src/databasestest/DatabasesTest.java
+++ src/databasestest/DatabasesTest.java
@@ -15,7 +15,10 @@ public class DatabasesTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        MainWindow newWindow = new MainWindow();
+        newWindow.setVisible(true);
+        
     }
     
 }
